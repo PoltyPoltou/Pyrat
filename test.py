2f3bea83456a75a598a882627d9a9e990bd287a9
Nicolas Pierre <pierrenicolas.doumer@laposte.net>
2019-09-03 18:27:44 +0200

    Revert "Revert "first push""
    This reverts commit 0ac1944e43a31bd505b03c2c99d80d52189aabb0.

---
+++ test.py
@@ -0,0 +1,18 @@
+import random
+foo = 2
+MOVE_DOWN = 'D'
+MOVE_LEFT = 'L'
+MOVE_RIGHT = 'R'
+MOVE_UP = 'U'
+
+
+def fun():
+    foo = 10
+    print(foo)
+
+
+fun()
+
+print(random.choice([MOVE_DOWN, MOVE_RIGHT, MOVE_UP, MOVE_LEFT]))
+print(random.choice([MOVE_DOWN, MOVE_RIGHT, MOVE_UP, MOVE_LEFT]))
+print(random.choice([MOVE_DOWN, MOVE_RIGHT, MOVE_UP, MOVE_LEFT]))

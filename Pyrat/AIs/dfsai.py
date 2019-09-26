0c47203148d1945708366268c2dab804c377a85b
Nicolas Pierre <polt.gun@gmail.com>
2019-09-27 01:20:42 +0200

    dfs stats

---
+++ Pyrat/AIs/dfsai.py
@@ -0,0 +1,64 @@
+###############################
+# Please put your imports here
+from collections import deque
+import test as t
+###############################
+# Please put your global variables here
+
+MOVE_DOWN = 'D'
+MOVE_LEFT = 'L'
+MOVE_RIGHT = 'R'
+MOVE_UP = 'U'
+directions = {(0, 1): MOVE_UP, (0, -1): MOVE_DOWN,
+              (1, 0): MOVE_RIGHT, (-1, 0): MOVE_LEFT}
+path = []
+
+
+def getDirection(direction, position):
+    (fstA, sndA) = direction
+    (fstB, sndB) = position
+    fst = fstA - fstB
+    snd = sndA-sndB
+    return directions[(fst, snd)]
+
+# allows to use couple to access data in a matrix
+
+
+def coupleToIndex(matrix, couple):
+    (a, b) = couple
+    return matrix[a][b]
+
+
+def targetNextCheese(playerPos, mazeMap, piecesOfCheese):
+    queue = []
+    fatherDic = {playerPos: (-1, -1)}
+    queue.append(playerPos)
+    cheeseFound = False
+    while queue != [] and not cheeseFound:
+        vertice = queue.pop()
+        for elmt in mazeMap[vertice].keys():
+            if elmt not in fatherDic.keys():
+                fatherDic.update({elmt: vertice})
+                queue.append(elmt)
+            if elmt in piecesOfCheese:
+                cheeseFound = True
+                destination = elmt
+                break
+    iterator = destination
+    path.append(destination)
+    while fatherDic[iterator] != playerPos:
+        path.append(fatherDic[iterator])
+        iterator = path[-1]
+
+
+def preprocessing(mazeMap, mazeWidth, mazeHeight, playerLocation, opponentLocation, piecesOfCheese, timeAllowed):
+    targetNextCheese(playerLocation, mazeMap, piecesOfCheese)
+    return
+
+
+def turn(mazeMap, mazeWidth, mazeHeight, playerLocation, opponentLocation, playerScore, opponentScore, piecesOfCheese, timeAllowed):
+    if path == []:
+        targetNextCheese(playerLocation, mazeMap, piecesOfCheese)
+    objectif = path.pop()
+    return getDirection(objectif, playerLocation)
+    
--- Pyrat/data.bat
+++ Pyrat/data.bat
@@ -4,5 +4,5 @@ C:
 set /A j = 1
 :loop
 set /A j = %j% +2 
-start "%j%" cmd /K "python pyrat.py --rat AIs/bfsai.py -x %j% -y %j% -p 1 -md 0 -d 0 --nodrawing"
+start "%j%" cmd /K "python pyrat.py --rat AIs/dfsai.py -x %j% -y %j% -p 1 -md 0 -d 0 --nodrawing"
 if %j% == 25 (pause) else (goto :loop)

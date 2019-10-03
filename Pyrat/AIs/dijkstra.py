20e78703ad724d80b7de7ff1085a5b45927b28f7
Nicolas PIERRE <pierrenicolas.doumer@laposte.net>
2019-10-03 19:26:51 +0200

    fix of opt and documentation on dijkstra

--- Pyrat/AIs/dijkstra.py
+++ Pyrat/AIs/dijkstra.py
@@ -15,7 +15,10 @@ directions = {(0, 1): MOVE_UP, (0, -1): MOVE_DOWN,
 path = []
 
 
-def getDirection(direction, position):
+def getDirection(direction: tuple, position: tuple) -> str:
+    '''
+    get two positions and return the move to go from position to direction
+    '''
     (fstA, sndA) = direction
     (fstB, sndB) = position
     fst = fstA - fstB
@@ -25,23 +28,37 @@ def getDirection(direction, position):
 # allows to use couple to access data in a matrix
 
 
-def coupleToIndex(matrix, couple):
+def coupleToIndex(matrix: list, couple: tuple):
+    '''
+get a list of list, matrix, and a couple and considers them as coordinates 
+'''
     (a, b) = couple
     return matrix[a][b]
 
 
-def targetNextCheese(playerPos, mazeMap, piecesOfCheese, mazeHeight, mazeWidth):
+def targetNextCheese(playerPos: tuple, mazeMap: dict, piecesOfCheese: list, mazeHeight: int, mazeWidth: int) -> list:
+    '''
+    WARNING it has side effects on path !
+    mazeMap is the dict discribing the map
+    piecesOfCheese the list of the coordinates of the remaining cheeses
+    mazeWidth and mazeHeight are the dimensions of the maze
+    from the the player's position, returns the path to the closest cheese
+    and stores it in the global variable path
+    '''
+    # setup of dijkstra algorithm
     heap = []
     fatherDic = {playerPos: (-1, -1)}
     heapq.heappush(heap, (0, playerPos))
     length = [[float('inf') for i in range(mazeHeight)]
               for i in range(mazeWidth)]
     length[0][0] = 0
+    # loop while we do not have explored every node
     while heap != []:
         (weight, vertice) = heapq.heappop(heap)
         for elmt in mazeMap[vertice].keys():
             x, y = elmt
             if coupleToIndex(length, elmt) > weight + mazeMap[vertice][elmt]:
+                # affects the new weight if it is better than the previous one
                 if elmt in heap:
                     heap[heap.index((coupleToIndex(length, elmt), elmt))] = (
                         weight + mazeMap[vertice][elmt], elmt)
@@ -49,6 +66,7 @@ def targetNextCheese(playerPos, mazeMap, piecesOfCheese, mazeHeight, mazeWidth):
                 else:
                     length[x][y] = weight + mazeMap[vertice][elmt]
                     heapq.heappush(heap, (length[x][y], elmt))
+                # adds or update the father's elmt, which is vertice
                 fatherDic.pop(elmt, True)
                 fatherDic.update({elmt: vertice})
     destination = piecesOfCheese[0]
